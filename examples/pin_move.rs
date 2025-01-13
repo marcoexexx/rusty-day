0f5cef01f2dad71f13391f6742c4bd1decc9a63e
Aung Koko Lwin <toyko2001@gmail.com>
2025-01-13 15:13:18 +0630

    fmt

--- examples/pin_move.rs
+++ examples/pin_move.rs
@@ -3,15 +3,15 @@
 use std::pin::Pin;
 
 fn is_unpin<T: Unpin>(data: T) -> bool {
-    true
+  true
 }
 
 fn prevent_move_on_stack() {
-    let data = String::from("hello"); // `String` is movable.
-    let pinned = Pin::new(&data); // Pinned reference, but doesn't truly prevent moves.
+  let data = String::from("hello"); // `String` is movable.
+  let pinned = Pin::new(&data); // Pinned reference, but doesn't truly prevent moves.
 
-    // Correct usage: we cannot move `data` through `pinned`,
-    // but `data` itself can still be moved by its own value.
+  // Correct usage: we cannot move `data` through `pinned`,
+  // but `data` itself can still be moved by its own value.
 }
 
 trait Todo {}
@@ -21,14 +21,13 @@ struct Cat;
 impl Todo for Cat {}
 
 fn prevent_move_on_heap() {
-    let data = Cat; // movable
-                    // `Todo` trait is unknown size, so dyn allocate in heap
-    let pinned: Pin<Box<dyn Todo>> = Box::pin(data);
+  let data = Cat; // movable
+                  // `Todo` trait is unknown size, so dyn allocate in heap
+  let pinned: Pin<Box<dyn Todo>> = Box::pin(data);
 
-    // compile error: cannot move
-    // let moved = *pinned;
-    // let moved = Pin::into_inner(pinned);  // Cannot safely move inner value, dyn Todo is Unpin
+  // compile error: cannot move
+  // let moved = *pinned;
+  // let moved = Pin::into_inner(pinned);  // Cannot safely move inner value, dyn Todo is Unpin
 }
 
-fn main() {
-}
+fn main() {}
--- examples/self-ref.rs
+++ examples/self-ref.rs
@@ -1,52 +1,53 @@
-use core::{marker::PhantomPinned, pin::Pin};
+use core::marker::PhantomPinned;
+use core::pin::Pin;
 
 struct SelfRef {
-    data: String,
-    point: *const String,
-    _marker: PhantomPinned,
+  data: String,
+  point: *const String,
+  _marker: PhantomPinned,
 }
 
 impl SelfRef {
-    fn new(data: String) -> Self {
-        SelfRef {
-            data,
-            point: std::ptr::null(),
-            _marker: PhantomPinned,
-        }
+  fn new(data: String) -> Self {
+    SelfRef {
+      data,
+      point: std::ptr::null(),
+      _marker: PhantomPinned,
     }
+  }
 
-    fn _set_data(&mut self, data: String) {
-        self.data = data
-    }
+  fn _set_data(&mut self, data: String) {
+    self.data = data
+  }
 
-    fn _init(&mut self) {
-        self.point = &self.data as *const String;
-    }
+  fn _init(&mut self) {
+    self.point = &self.data as *const String;
+  }
 
-    fn set_data(self: Pin<&mut Self>, data: String) {
-        unsafe { self.get_unchecked_mut().data = data }
-    }
+  fn set_data(self: Pin<&mut Self>, data: String) {
+    unsafe { self.get_unchecked_mut().data = data }
+  }
 
-    fn init(self: Pin<&mut Self>) {
-        let ptr = &self.data as *const String;
-        unsafe {
-            self.get_unchecked_mut().point = ptr;
-        }
+  fn init(self: Pin<&mut Self>) {
+    let ptr = &self.data as *const String;
+    unsafe {
+      self.get_unchecked_mut().point = ptr;
     }
+  }
 
-    fn locate(&self) {
-        unsafe { println!("{:p} -> {}", self.point, *self.point) }
-    }
+  fn locate(&self) {
+    unsafe { println!("{:p} -> {}", self.point, *self.point) }
+  }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut data = Box::pin(SelfRef::new("hello".into()));
+  let mut data = Box::pin(SelfRef::new("hello".into()));
 
-    data.as_mut().init();
-    data.locate();
+  data.as_mut().init();
+  data.locate();
 
-    data.as_mut().set_data("world".into());
-    data.locate();
+  data.as_mut().set_data("world".into());
+  data.locate();
 
-    Ok(())
+  Ok(())
 }
